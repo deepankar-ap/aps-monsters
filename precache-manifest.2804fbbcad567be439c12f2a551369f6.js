e23e4dd269f4e6a86855f4f0110e3be97afea42d
deepankar-ap <deepankar@appperfect.com>
2020-03-30 18:40:37 +0530

    Updates

---
+++ precache-manifest.2804fbbcad567be439c12f2a551369f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29982eece1c9325a23bdf3e6be82b944",
+    "url": "/aps-monsters/index.html"
+  },
+  {
+    "revision": "206308d3960db35a20f4",
+    "url": "/aps-monsters/static/css/main.c0f44309.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/aps-monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aps-monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206308d3960db35a20f4",
+    "url": "/aps-monsters/static/js/main.521b0298.chunk.js"
+  },
+  {
+    "revision": "16d6efb0e4dbed0deca6",
+    "url": "/aps-monsters/static/js/runtime-main.cc235489.js"
+  }
+]);
